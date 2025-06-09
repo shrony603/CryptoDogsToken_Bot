9cc0877ad93d589d0877d5ec21c548c8408011d7
shrony603 <148349283+shrony603@users.noreply.github.com>
2025-06-10 03:39:56 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 from telebot import TeleBot, types
 
+# BotFather থেকে নেওয়া Token
 TOKEN = "8199006545:AAHHmZD2Tj2TOTNj7Ig2flD9agm_5rMuqBk"
 bot = TeleBot(TOKEN)
 
-# Replace this with your referral link
+# Monotag / Shortener লিঙ্ক
 referral_url = "https://otieu.com/4/9431817"
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     chat_id = message.chat.id
-    ref_id = message.text.split(" ")[1] if len(message.text.split()) > 1 else None
 
-    if ref_id:
+    # রেফারার আইডি চেক
+    args = message.text.split()
+    if len(args) > 1:
+        ref_id = args[1]
         bot.send_message(chat_id, f"👋 Welcome! You were referred by user ID: {ref_id}")
     else:
         bot.send_message(chat_id, "👋 Welcome to Crypto Dogs Token Bot!")
 
+    # মেসেজ পাঠানো হচ্ছে রেফারেল লিংকসহ
     msg = f"""🎯 Click here to earn tokens:
 👉 {referral_url}
 
 💰 Share your referral link and earn more!
-📲 Your referral link: https://t.me/CryptoDogsEarningBot?start={chat_id}
+📲 Your referral link:
+https://t.me/CryptoDogsEarningBot?start={chat_id}
 """
     bot.send_message(chat_id, msg)
 
